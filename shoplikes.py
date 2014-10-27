8dd00ef79ea78762446775fb0341c3d122735a39
Parag Dulam <paragdulam@gmail.com>
2014-10-27 20:53:39 +0530

    Ebay Integration Done with Table with Scrolling Remaining.

--- shoplikes.py
+++ shoplikes.py
@@ -1,11 +1,11 @@
 from flask import Flask,render_template,request,url_for
-import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def index(name=None):
 	return render_template("index.html",name=name)
+
 
 if __name__ == '__main__':
 	app.run()
