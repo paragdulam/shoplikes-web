b5e949e15a21c9fcabb60b6f42bd636c5d6e0f61
sandeep <sandeep@sandeep.(none)>
2014-11-05 21:13:45 +0530

    removed debug

--- shoplikes.py
+++ shoplikes.py
@@ -30,5 +30,5 @@ if __name__ == '__main__':
 	 # Bind to PORT if defined, otherwise default to 5000.
     #port = int(os.environ.get('PORT', 5000))
     port = int(os.environ.get('PORT', 33507))
-    app.debug = True
+    #app.debug = True
     app.run(host='0.0.0.0', port=port)
