5ab59959eb7931d2c2dd143e6c5b844fe0624933
sandeep <sandeep@sandeep.(none)>
2014-11-05 21:27:54 +0530

    revert function

--- shoplikes.py
+++ shoplikes.py
@@ -13,15 +13,15 @@ def index(name=None):
 	return render_template("index.html",name=name)
 
 
-# @app.route("/products", methods=['POST'])
-# def getProductsforLike():
-# 	params = ast.literal_eval(request.data)
-# 	likeId = params['likeId']
-# 	likeName = params['likeName']
-# 	requestURL = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=SapnaSol-b016-439b-ba9f-0a88df89de2e&RESPONSE-DATA-FORMAT=JSON&keywords='+likeName+'&outputSelector(0)=galleryPlusPictureURL&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice'
-# 	response = requests.get(requestURL).content
-# 	retval = {'likeId':likeId, 'likeName':likeName, 'results':response}
-# 	return jsonify(retval)
+@app.route("/products", methods=['POST'])
+def getProductsforLike():
+	params = ast.literal_eval(request.data)
+	likeId = params['likeId']
+	likeName = params['likeName']
+	requestURL = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=SapnaSol-b016-439b-ba9f-0a88df89de2e&RESPONSE-DATA-FORMAT=JSON&keywords='+likeName+'&outputSelector(0)=galleryPlusPictureURL&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice'
+	response = requests.get(requestURL).content
+	retval = {'likeId':likeId, 'likeName':likeName, 'results':response}
+	return jsonify(retval)
 
 
 
