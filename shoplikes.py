f8067065204f54efd669be9d0663b7512d2c1e91
Parag Dulam <paragdulam@gmail.com>
2015-06-21 15:04:33 +0530

    feature: save logged use to parse

--- shoplikes.py
+++ shoplikes.py
@@ -13,6 +13,22 @@ def index(name=None):
 	return render_template("index.html",name=name)
 
 
+@app.route("/users",methods=['POST'])
+def saveUser():
+	params = ast.literal_eval(request.data)
+	#username = params['username']
+	#email=params['email']
+	headers = {
+		'Content-Type' : 'application/json',
+		'X-Parse-Application-Id' : 'SbKaM253JsYL8N1TniREUhYTtwuNiFuX1ctxYXXZ',
+		'X-Parse-REST-API-Key' : 'cFfHwPpPKTevmeNKPGsZiq0Svpb9mXa6ufx3J9uW'
+	}
+	url = "https://api.parse.com/1/classes/WebUser"
+	r = requests.post(url, data=request.data, headers = headers ,allow_redirects=False)
+	print r
+	return jsonify(result={"status": request.data })
+
+
 @app.route("/products", methods=['POST'])
 def getProductsforLike():
 	params = ast.literal_eval(request.data)
@@ -32,6 +48,7 @@ def getProductsforLike():
 if __name__ == '__main__':
 	 # Bind to PORT if defined, otherwise default to 5000.
     #port = int(os.environ.get('PORT', 5000))
+
     port = int(os.environ.get('PORT', 33507))
     #app.debug = True
     app.run(host='0.0.0.0', port=port)
